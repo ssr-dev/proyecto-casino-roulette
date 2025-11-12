187a13cea7be8af78e4d1fba512b556ececc93ac
Jennifer Granados Nossa <jennifer.granados@uptc.edu.co>
2025-11-12 13:22:40 -0500

    Numeros calientes y frios funcionando

--- backend/src/main/java/com/casino/dtos/SpinResult.java
+++ backend/src/main/java/com/casino/dtos/SpinResult.java
@@ -8,5 +8,24 @@ import lombok.Data;
 public class SpinResult {
     private int winningNumber;
     private boolean spin;
+	public SpinResult(int winningNumber, boolean spin) {
+		super();
+		this.winningNumber = winningNumber;
+		this.spin = spin;
+	}
+	public int getWinningNumber() {
+		return winningNumber;
+	}
+	public void setWinningNumber(int winningNumber) {
+		this.winningNumber = winningNumber;
+	}
+	public boolean isSpin() {
+		return spin;
+	}
+	public void setSpin(boolean spin) {
+		this.spin = spin;
+	}
+    
+    
 
 }
